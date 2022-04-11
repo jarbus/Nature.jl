02dcd5f79cae54e382bae25f388eed92551b4e6f
Jack <jarbus@tutanota.com>
2022-04-11 17:32:46 -0400

    cluster tweaks

--- batch-template.sh
+++ batch-template.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=test
-#SBATCH --output=test.out
+#SBATCH --job-name=OEC
+#SBATCH --output=run.out
 #SBATCH --account=guest
 #SBATCH --partition=guest-gpu
-#SBATCH --time=02:00:00
+#SBATCH --qos=low-gpu
+#SBATCH --time=24:00:00
 #SBATCH --gres=gpu:TitanX:1   # Request 1 TitanX GPU
  
 # Load modules required for your job
-module load cuda/9.0
-module load anaconda
+source /home/garbus/.bashrc
+conda activate julia
  
 # Command to execute
