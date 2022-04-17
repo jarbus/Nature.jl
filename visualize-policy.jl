22aa3b5afce41dc9f5ac5dcee9695ef4339824ff
Jack <jarbus@tutanota.com>
2022-04-17 10:37:46 -0400

    visualize better checkpoints

--- visualize-policy.jl
+++ visualize-policy.jl
@@ -5,18 +5,27 @@ pols = readdir("checkpoints/")
 for (i, pol) in enumerate(pols)
     println(i, ". ", pol)
 end
-print("Select a policy: ")
+print("Select a run: ")
 pol_name = "checkpoints/"*pols[parse(Int, readline())]
 
-p = deserialize(pol_name)
+pols = readdir("$pol_name/")
+for (i, pol) in enumerate(pols)
+    println(i, ". ", pol)
+end
+print("Select a checkpoint: ")
+pol_name = "$pol_name/"*pols[parse(Int, readline())]
 
-env = NatureEnv(num_starting_players=36,
-                world_size=(64, 64),
+p = deserialize(pol_name)[:policy]
+
+WORLD_SIZE=(2,2)
+env = NatureEnv(num_starting_players=2,
+                world_size=WORLD_SIZE,
                 window = 3,
-                episode_len=500,
+                num_frames=2,
+                episode_len=1000,
                 vocab_size=5,
                 food_generators=[
-                    FoodGen([15,15],[60,60]),
-                    FoodGen([45,45],[60,60]),
+                    FoodGen([1,1],[0.0001,0.0001],max_pos=WORLD_SIZE),
+                    FoodGen([2,2],[0.0001,0.0001],max_pos=WORLD_SIZE),
                    ])
 step_through_env(env, p)
