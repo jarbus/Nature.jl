4f83e808758f2acc40c32fb48cced125fad668f2
Jack <jarbus@tutanota.com>
2022-04-17 10:21:32 -0400

    food balance hooks

--- src/hooks.jl
+++ src/hooks.jl
@@ -2,6 +2,7 @@ mutable struct NatureHook10 <: AbstractHook
     step::Int
     max_steps::Int
     food_counts::Vector{Float32}
+    food_balance::Vector{Float32}
     act_counts::Vector{Int}
     act_probs::Vector{Float32}
     player_acts::Vector{Vector{Int}}
@@ -12,6 +13,7 @@ function NatureHook10(env::NatureEnv, trial_id::String, max_steps::Int)
     NatureHook(0,
         max_steps,
         zeros(Float32, env.food_types),
+        [],
         zeros(length(action_space(env, 1))),
         Vector{Float32}(),
         [zeros(length(action_space(env, 1))) for i in 1:length(env.players)],
@@ -33,6 +35,7 @@ function (hook::NatureHook)(::PostActStage, policy, env)
     for p in 1:length(env.players)
         hook.total_rewards[p] += reward(env, p)
         hook.food_counts = hook.food_counts .+ env.players[p].food_counts
+        push!(hook.food_balance, max(env.players[p].food_counts...) / min(env.players[p].food_counts...))
     end
     hook.step += 1
     steps_per_checkpoint = 1_000
@@ -50,20 +53,22 @@ end
 
 function (hook::NatureHook)(::PostEpisodeStage, policy, env)
 
-    @info "episode"     len=env.step                       log_step_increment=0
-    @info "total_food"  total_food=Tuple(hook.food_counts) log_step_increment=0
-    @info "actions"     act=Tuple(hook.act_counts)         log_step_increment=0
-    @info "rewards"     reward=Tuple(hook.total_rewards)   log_step_increment=0
-    @info "rewards"     total_reward=sum(hook.total_rewards)
-    @info "act_prob"    act_prob=mean(hook.act_probs)      log_step_increment=0
+    @info "episode"     len=env.step                         log_step_increment=0
+    @info "total_food"  total_food=Tuple(hook.food_counts)   log_step_increment=0
+    @info "actions"     act=Tuple(hook.act_counts)           log_step_increment=0
+    @info "rewards"     reward=Tuple(hook.total_rewards)     log_step_increment=0
+    @info "rewards"     total_reward=sum(hook.total_rewards) log_step_increment=0
+    @info "act_prob"    act_prob=mean(hook.act_probs)        log_step_increment=0
     @info "player_acts" player_acts=Tuple([Tuple(hook.player_acts[p]) for p in 1:length(env.players)]) log_step_increment=0
-    @info "exchanges" ex=Tuple(env.exchanges)
+    @info "exchanges"   ex=Tuple(env.exchanges)              log_step_increment=0
+    @info "food_balance" food_balance=mean(hook.food_balance)
 
 
     hook.act_counts    = zeros(length(action_space(env, 1)))
     hook.total_rewards = zeros(length(env.players))
     hook.food_counts   = zeros(env.food_types)
-    hook.act_probs      = Vector{Float32}()
+    hook.act_probs     = Vector{Float32}()
+    hook.food_balance  = []
 
     hook.player_acts = [zeros(length(action_space(env, 1))) for _ in env.players]
 end
